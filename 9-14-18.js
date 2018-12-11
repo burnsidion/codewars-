ff69ebb54f7d5a3a136589f12936fd8d53c6ec3c
Ian Burnside <ian.burnside89@gmail.com>
2018-12-11 15:57:07 -0700

    more finished katas

---
+++ 9-14-18.js
@@ -0,0 +1,29 @@
+/*Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+
+Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type. */
+
+
+function dbSort(a){
+  let strings = a.filter(x => typeof x == 'string').sort();
+  let nums = a.filter(x => typeof x == 'number').sort((a,b) => a - b);
+  return nums.concat(strings)
+}
+
+
+/*You want to personalise the emails so that the people don't think they are being spammed!
+You will be given a string of marketing text called campaign. In the string, there will be personalisable elements like "<NAME>", "<CITY>", "<FAVOURITE PRODUCTS>".
+You need to replace these elements with the personal details of the person to whom you are sending the email. You will receive this information in a hash called person, which may contain other information from the database, which you don't need. */
+
+function personalise( campaign, person){
+  campaign = campaign.replace(/<NAME>/g, person.name);
+  campaign = campaign.replace(/<CITY>/g, person.city);
+  campaign = campaign.replace(/<FAVOURITE PRODUCTS>/g, person.favourite_products)
+  return campaign
+}
+
+//Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+//For correct answer program should return int from 0 to 9.
+
+function getAge(inputString){
+return parseInt(inputString)
+}
