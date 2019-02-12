25457c28e8cdbcba1324b174156148a3d3353a50
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2019-02-12 14:18:10 -0700

    still working on this one

--- 2-05-19.js
+++ 2-05-19.js
@@ -1,40 +1,48 @@
-
 //Question 1
-let dates = ['Oct 7, 2009', 'Nov 10, 2009', 'Jan 10, 2009', 'Oct 22, 2009', 'Feb 12, 2009', 'Dec 13, 2009'];
+let dates = [
+  'Oct 7, 2009',
+  'Nov 10, 2009',
+  'Jan 10, 2009',
+  'Oct 22, 2005',
+  'Feb 12, 2006',
+  'Dec 13, 2006'
+];
 
-function orderDates(dates){
+function orderDates(dates) {
   let dateObj = {
-    'Jan':1,
-    'Feb':2,
-    'Mar':3,
-    'Apr':4,
-    'May':5,
-    'Jun':6,
-    'Jul':7,
-    'Aug':8,
-    'Sep':9,
-    'Oct':10,
-    'Nov':11,
-    'Dec':12
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Aug': 8,
+    'Sep': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dec': 12
   };
   let finDate = [];
   let months = [];
   let days = [];
-  for(let i = 0; i < dates.length; i++){
-      dates[i] = dates[i].split(' ');
-      let newDate = [dates[i][2],dateObj[dates[i][0]],dates[i][1]];
-      finDate.push(newDate);
+  let theEnd = [];
+  for(let i = 0; i < dates.length; i++) {
+    dates[i] = dates[i].split(' ');
+    let newDate = [
+      dates[i][2],
+      dateObj[dates[i][0]],
+      dates[i][1].replace(/,/g, '')
+    ];
+    finDate.push(newDate);
   }
-  //loop through finDate array to pull out months and days and sort descending
+    finDate.sort((a, b) => b[0] - a[0])
   for(let j = 0; j < finDate.length; j++){
-      months.push(finDate[j][1]);
-      days.push(parseInt(finDate[j][2]))
-    }
-    console.log("MONTHS = " ,months.sort((a,b) => b - a))
-    console.log("DAYS = ", days.sort((a,b) => b - a))
+      months.push(finDate[j][1])
+      days.push(finDate[j][2])
   }
 
-
+}
 
 console.log(orderDates(dates));
 
