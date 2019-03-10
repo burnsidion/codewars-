4d5665dd32c1d2e80e40b476955a6b2fb0c35441
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2019-03-10 15:58:02 -0600

    couple more today

---
+++ 3-10-19.js
@@ -0,0 +1,31 @@
+// Complete the function which takes two arguments and returns all numbers which are divisible by given divisor. First argument is an array of numbers and the second is the divisor.
+//
+// Example
+// divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+// divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+
+
+function divisibleBy(numbers, divisor){
+  return numbers.filter(x => x % divisor == 0);
+}
+
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1 (in C#, N might be less then 1).
+//
+// C# ONLY: If N is smaller then or equal to 0, throw an ArgumentOutOfRangeException!
+// Replace certain values however if any of the following conditions are met:
+//
+// If the value is a multiple of 3: use the value 'Fizz' instead
+// If the value is a multiple of 5: use the value 'Buzz' instead
+// If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+// C# method calling example:
+//
+// string[] result = FizzBuzz.GetFizzBuzzArray(3); // => [ "1", "2", "Fizz" ]
+
+function fizzbuzz(n){
+  let arr = [];
+  for(let i = 1; i <= n; i++){
+    arr.push(i)
+  }
+  return arr.map(x => (x % 15 === 0) ? "FizzBuzz" : (x % 3 === 0) ? "Fizz" : (x % 5 == 0) ? "Buzz" : x);
+}
