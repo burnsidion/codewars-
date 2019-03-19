db41d69351a07d9afafc636a1122e08f2757b583
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2019-03-18 19:29:35 -0600

    technical take home

---
+++ 3-18-19.js
@@ -0,0 +1,35 @@
+let arr = [
+  {
+    first: "Amanda",
+    last: "Byron",
+    group: "Sales"
+  }, {
+    first: "Ye",
+    last: "Xia",
+    group: "Receiving",
+    nameOrder: "reverse"
+  }, {
+    first: "Miltiades",
+    last: "Crescens",
+    group: "Sales"
+  }
+]
+  // need to make each array an array of objects
+  let obj = {};
+  let key;
+  for(let i = 0; i < arr.length; i++){
+     let fullName = `${arr[i].first} ${arr[i].last}`;
+     key = arr[i].group;
+     if(arr[i].nameOrder && arr[i].nameOrder.toLowerCase() === 'reverse'){
+       fullName = `${arr[i].last} ${arr[i].first}`;
+     }
+     if(obj[key]){
+       obj[key].push({name: fullName})
+     } else {
+       obj[key] = [{name:fullName}]
+     }
+     //check for reverse
+
+
+  }
+  console.log(obj)
